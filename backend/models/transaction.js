cdd28a7f8d86d044d5b817c443de84d066d87dca
Abdul <ajabdul0786@gmail.com>
2025-07-05 20:49:45 +0530

    Fixed render problem

--- backend/models/transaction.js
+++ backend/models/transaction.js
@@ -1,10 +1,10 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const transactionSchema = new mongoose.Schema({
   amount: { type: Number, required: true },
   date: { type: String, required: true },
   description: { type: String, required: true },
-  category: { type: String, required: true }
+  category: { type: String, required: true },
 });
 
-module.exports = mongoose.model('Transaction', transactionSchema);
+module.exports = mongoose.model("Transaction", transactionSchema);
